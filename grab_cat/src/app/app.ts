d8a593949fb6831d60e85081a45c9adb22753e07
Rod Kristian Rebarbas <50227721+kryss143@users.noreply.github.com>
2026-04-24 10:12:09 +0800

    Initial commit and push

---
+++ grab_cat/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Home } from './home/home';
+import { Navbar } from './navbar/navbar';
+import { Footer } from './footer/footer';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Home, Navbar, Footer],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App {}
